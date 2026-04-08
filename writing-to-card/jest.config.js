f9f26fd2406c53210aee1db5ee4348a48a6cf43e
Lan-zk <l785101304@Gmail.com>
2026-04-08 23:20:24 +0800

    feat(writing-to-card): add project scaffold with TypeScript, Jest, ESLint

---
+++ writing-to-card/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/evals'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    global: { branches: 70, functions: 70, lines: 70, statements: 70 }
+  }
+};
